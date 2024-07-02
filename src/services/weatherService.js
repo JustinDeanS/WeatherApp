9e6e31c30e1bc7ba3219e20888ed9ccc040e52e9
JustinDeanS <143033461+JustinDeanS@users.noreply.github.com>
2024-07-02 18:49:44 -0400

    fixed base_url

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;  // Replace 'YOUR_API_KEY' with your actual API key
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/';
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/';
 
 export const getWeather = async (city) => {
   const response = await axios.get(`${BASE_URL}weather?q=${city}&appid=${API_KEY}&units=metric`);
